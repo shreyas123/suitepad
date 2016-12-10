72615973819ef645cee53e218ca6a88cb13d6020
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-10 22:42:10 +0100

    Add a action to return JSON of a random quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,8 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.random
   end
+
+  def random_quote
+    render json: Quote.random
+  end
 end
--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe QuotesController, type: :controller do
       expect(assigns(:quote)).to eql quote
     end
   end
+
+  describe 'GET random_quote' do
+    let!(:quote) { create :quote }
+    subject(:request) { get :random_quote, format: :json }
+
+    it { is_expected.to be_success }
+
+    it 'returns the JSON object of the quote' do
+      request
+      expect(JSON.parse(response.body)).to eql(JSON.parse(quote.to_json))
+    end
+  end
 end
