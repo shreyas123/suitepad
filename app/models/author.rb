ae6f171b96ca1ddb2fb83e464e9044f7a63c56db
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-10 19:38:04 +0100

    Add validation for uniqueness

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
 
-validates :name, presence: true
+validates :name, presence: true, uniqueness: true
 
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
