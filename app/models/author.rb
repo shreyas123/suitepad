dddd5d44392ddef099df18f91c4241193f575aa8
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-10 19:40:15 +0100

    Add case_sensitive false to avoid duplicate names

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
 
-validates :name, presence: true, uniqueness: true
+validates :name, presence: true, uniqueness: { case_sensitive: false }
 
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
 end
