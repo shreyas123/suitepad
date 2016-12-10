ee89924fbdb6790371c4e9f10920fa327f383868
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2016-12-10 19:35:02 +0100

    Add Should matchers gem and modify rails_helper for use of shoulda matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,13 @@ require 'rspec/rails'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
